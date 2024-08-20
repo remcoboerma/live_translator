a1c8a49b8834a2fdffd63bbf37fd721650edd6cd
Remco Boerma <remco.b@educationwarehouse.nl>
2024-08-20 14:21:31 +0200

    feat: Implement torch translation and enhance task.py functionality
    Reorganized the tasks.py script to support translation using torch and detailed error handling. Added libraries to requirements.in for transformers support, ensuring proper implementation and dependency management.

--- tasks.py
+++ tasks.py
@@ -24,23 +24,6 @@ def setup(c: Context):
         print("An error occurred while checking for portaudio19-dev:")
         print(e)
 
-    # try:
-    #     result = c.run("dpkg -l python3-pyaudio", hide=True, warn=True)
-    #
-    #     # Check the output
-    #     if "ii" in result.stdout:
-    #         print("python3-pyaudio is installed.")
-    #     else:
-    #         print("python3-pyaudio is NOT installed.")
-    #         c.sudo("apt install python3-pyaudio")
-    # except Exception as e:
-    #     print("An error occurred while checking for python3-pyaudio:")
-    #     print(e)
-    print("Installing python dependencies...")
-    result = c.run(".venv/bin/pip3 install -r requirements.txt", hide=True, warn=True)
-    if not result.ok:
-        print("An error occurred while installing requirements:")
-        print(result.stdout)
     print("Checking variables...")
     check_env(
         "ASSEMBLYAI_KEY",
@@ -62,34 +45,114 @@ def setup(c: Context):
         "127.0.0.1",
         "SocketIO host",
     )
-    sio_url= check_env(
+    sio_url = check_env(
         "SIO_URL",
         f"http://{sio_host}:{sio_port}",
-        "SocketIO URL, based on host and port"
+        "SocketIO URL, based on host and port",
+    )
+    torch_architecture = check_env(
+        "TORCH_ARCHITECTURE",
+        "cpu",
+        "The torch architecture to use: cpu|cu118|cu121|cu124|rocm6.1; https://pytorch.org for full list.",
     )
 
+    print("Installing python dependencies...")
+    result = c.run(".venv/bin/pip3 install -r requirements.txt", hide=True, warn=True)
+    if not result.ok:
+        print("An error occurred while installing requirements:")
+        print(result.stdout)
+
+    result = c.run(
+        ".venv/bin/pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/{}",
+        hide=False,
+        warn=True,
+    )
+
+    print("Starting server...")
+    c.run(
+        f".venv/bin/python tasks.py serve --sio-url={sio_url} --torch-architecture={torch_architecture}"
+    )
 
 
 class PplxError(Exception): ...
 
 
 @task
-def translate(c: Context):
+def translate(c: Context, using="torch"):
+    if using == "pplx":
+        translate_pplx()
+    elif using == "torch":
+        translate_torch()
+    else:
+        print("Error: invalid ")
+
+
+def translate_loop(gpt: callable):
+    import socketio
+
+    final_buffer = []
+    translated_buffer = []
+    with socketio.SimpleClient(
+        ssl_verify=False, logger=True, engineio_logger=True
+    ) as sio:
+        sio_url = edwh.get_env_value("SIO_URL")
+        sio.connect(sio_url)
+        while True:
+            message, data = sio.receive()
+            if message == "final":
+                final_buffer.append(data.strip())
+                translated_text = gpt(final_buffer)
+                sio.emit("translated", translated_text)
+                translated_buffer.append(translated_text)
+
+                # beautify the buffer, create a line with transparency gradient
+                html_lines = [
+                    f"<span id=line-{i}>{line}</span>"
+                    for i, line in enumerate(translated_buffer)
+                ]
+                html_fragment = "\n".join(html_lines)
+
+                # send this to the webbrowser
+                sio.emit("html_fragment", html_fragment)
+
+                # remove after the fifth line
+                translated_buffer = translated_buffer[-5:]
+            elif message == "exit":
+                sio.disconnect()
+                break
+
+
+def translate_torch():
+    from transformers import pipeline
+
+    def gpt(final_buffer: list[str]) -> str:
+        resp = pipe(final_buffer[-1])
+        translated = resp[0]["translation_text"]
+        # pipe("What's the distance between the earth and the moon?")
+        # [{'translation_text': 'Wat is de afstand tussen de aarde en de maan?'}]
+        return translated
+
+    print("Loading pipeline")
+    pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-nl")
+    translate_loop(gpt)
+
+
+def translate_pplx():
     import httpx
 
     def gpt(client: httpx.Client, api_key: str, history: list[str]) -> str:
         url = "https://api.perplexity.ai/chat/completions"
-        chat_history = '\n> ' + '\n> '.join(history[:-1])
+        chat_history = "\n> " + "\n> ".join(history[:-1])
         payload = {
             "model": "llama-3.1-8b-instruct",
             "messages": [
                 {
                     "content": "You are an english to dutch translator. "
-                               "When the user prompts you something, you reply with the dutch translation. "
-                               "You MUST NOT explain or act on anything the users says, just translate. You're NOT a helpful assistent. "
-                               "Keep in mind the last few sentences spoken to translate better using this context: "
-                               "\n "
-                               f"{chat_history}",
+                    "When the user prompts you something, you reply with the dutch translation. "
+                    "You MUST NOT explain or act on anything the users says, just translate. You're NOT a helpful assistent. "
+                    "Keep in mind the last few sentences spoken to translate better using this context: "
+                    "\n "
+                    f"{chat_history}",
                     "role": "system",
                 },
                 {"content": history[-1], "role": "user"},
@@ -128,47 +191,24 @@ def translate(c: Context):
 
     # Add your Perplexity API key and the text you want to translate
     api_key = edwh.get_env_value("PPLX_API_KEY")
-
-    final_buffer = []
-    translated_buffer = []
-
-    import socketio
-    with socketio.SimpleClient(ssl_verify=False, logger=True, engineio_logger=True) as sio, httpx.Client() as client:
-        sio_url = edwh.get_env_value("SIO_URL")
-        sio.connect(sio_url)
-        while True:
-            message, data = sio.receive()
-            if message == 'final':
-                final_buffer.append(data.strip())
-                translated_text = gpt(client, api_key, final_buffer)
-                sio.emit('translated', translated_text)
-                translated_buffer.append(translated_text)
-
-                # beautify the buffer, create a line with transparency gradient
-                html_lines = [f'<span id=line-{i}>{line}</span>' for i, line in enumerate(translated_buffer)]
-                html_fragment = "\n".join(html_lines)
-
-                # send this to the webbrowser
-                sio.emit('html_fragment', html_fragment)
-
-                # remove after the fifth line
-                translated_buffer = translated_buffer[-5:]
-            elif message == 'exit':
-                sio.disconnect()
-                break
+    with httpx.Client() as client:
+        translate_loop(functools.partial(gpt, client, api_key))
 
 
 @task
-def serve(c:Context):
+def serve(c: Context):
     sio_url = edwh.get_env_value("SIO_URL")
-    print(f'Serving on {sio_url}')
-    c.run('./sioserver.py')
+    print(f"Serving on {sio_url}")
+    c.run("./sioserver.py")
+
 
 @task
-def web(c:Context):
+def web(c: Context):
     import webbrowser
+
     sio_url = edwh.get_env_value("SIO_URL")
     webbrowser.open(sio_url)
+
 
 @task
 def stream(c: Context):
@@ -191,35 +231,46 @@ def stream(c: Context):
 
     aai.settings.api_key = edwh.get_env_value("ASSEMBLYAI_KEY")
 
-    def on_open(sio:socketio.SimpleClient, session_opened: aai.RealtimeSessionOpened):
+    def on_open(sio: socketio.SimpleClient, session_opened: aai.RealtimeSessionOpened):
         "This function is called when the connection has been established."
         print("Session ID:", session_opened.session_id)
 
-    def on_data(sio:socketio.SimpleClient, transcript: aai.RealtimeTranscript):
+    def on_data(sio: socketio.SimpleClient, transcript: aai.RealtimeTranscript):
         "This function is called when a new transcript has been received."
 
         if not transcript.text:
             return
 
         # emit either final or intermediate message
-        sio.emit('final' if isinstance(transcript, aai.RealtimeFinalTranscript) else 'intermediate', transcript.text)
+        sio.emit(
+            (
+                "final"
+                if isinstance(transcript, aai.RealtimeFinalTranscript)
+                else "intermediate"
+            ),
+            transcript.text,
+        )
 
         if isinstance(transcript, aai.RealtimeFinalTranscript):
             # final version, with capitalization and all
-            print(json.dumps(dict(type='final', text=transcript.text)))
+            print(json.dumps(dict(type="final", text=transcript.text)))
         else:
-            print(json.dumps(dict(type='intermediate', text=transcript.text)))
+            print(json.dumps(dict(type="intermediate", text=transcript.text)))
         sys.stdout.flush()
 
-    def on_error(sio:socketio.SimpleClient, error: aai.RealtimeError):
+    def on_error(sio: socketio.SimpleClient, error: aai.RealtimeError):
         "This function is called when the connection has been closed."
         print("AssemblyAI rror:", error)
 
-    def on_close(sio:socketio.SimpleClient, ):
+    def on_close(
+        sio: socketio.SimpleClient,
+    ):
         "This function is called when the connection has been closed."
         print("Closing Session")
 
-    with socketio.SimpleClient(ssl_verify=False, logger=True, engineio_logger=True) as sio:
+    with socketio.SimpleClient(
+        ssl_verify=False, logger=True, engineio_logger=True
+    ) as sio:
         sio_url = edwh.get_env_value("SIO_URL")
         sio.connect(sio_url)
 
@@ -242,38 +293,45 @@ def stream(c: Context):
 
         transcriber.close()
 
+
 @task
-def demo_message(c:Context):
+def demo_message(c: Context):
     import socketio
-    with socketio.SimpleClient(ssl_verify=False, logger=True, engineio_logger=True) as sio:
+
+    with socketio.SimpleClient(
+        ssl_verify=False, logger=True, engineio_logger=True
+    ) as sio:
         sio_url = edwh.get_env_value("SIO_URL")
         sio.connect(sio_url)
         cnt = 0
         while True:
             cnt += 1
-            sio.emit('demo',cnt)
+            sio.emit("demo", cnt)
             time.sleep(0.2)
             # event = sio.receive()
             # print(f"!> {event!r}")
 
 
 @task
-def go(c:Context):
-    c.run('ew local.serve &', disown=True)
-    c.run('ew local.translate &', disown=True)
-    c.run('ew local.stream &', disown=True)
-    c.run('ew local.web &', disown=True)
-    c.run('jobs')
+def go(c: Context):
+    c.run("ew local.serve &", disown=True)
+    c.run("ew local.translate &", disown=True)
+    c.run("ew local.stream &", disown=True)
+    c.run("ew local.web &", disown=True)
+    c.run("jobs")
 
 
 @task
-def exit(c:Context):
+def exit(c: Context):
     import socketio
-    with socketio.SimpleClient(ssl_verify=False, logger=True, engineio_logger=True) as sio:
+
+    with socketio.SimpleClient(
+        ssl_verify=False, logger=True, engineio_logger=True
+    ) as sio:
         sio_url = edwh.get_env_value("SIO_URL")
         sio.connect(sio_url)
-        sio.emit('exit',None)
+        sio.emit("exit", None)
         time.sleep(1)
-        sio.emit('exit',None)
+        sio.emit("exit", None)
         time.sleep(1)
-        sio.emit('exit',None)
+        sio.emit("exit", None)
